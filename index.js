9ee6fc12f5a39e2ff86970b5894209e37b6a77fb
Dennis Kemboi <cherogonydennis9@gmail.com>
2022-08-31 18:35:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random() + 1);
+console.log(random)
+
+let num3 = 144
+let num4 = 5
+const mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(7,12,20,4.5,9);
+console.log(max) 
